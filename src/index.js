18db67a88e081ce41a00f7344968aa03fa8a1c79
Stephen Zhou <hi@hyoban.cc>
2024-03-17 19:40:41 +0800

    feat: use bumpp to bump version

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import fs from 'node:fs'
-import path from 'node:path'
 
 import { versionBump } from 'bumpp'
 import conventionalRecommendedBump from 'conventional-recommended-bump'
-import fg from 'fast-glob'
-import gitSemverTags from 'git-semver-tags'
 import { Plugin } from 'release-it'
 import semver from 'semver'
-import { parse } from 'yaml'
 
 function hasAccess(path) {
   try {
@@ -18,8 +14,6 @@ function hasAccess(path) {
     return false
   }
 }
-
-const readJSON = file => JSON.parse(fs.readFileSync(file, 'utf8'))
 
 const prompts = {
   publish: {
@@ -34,7 +28,6 @@ const prompts = {
 
 const MANIFEST_PATH = './package.json'
 const MANIFEST_LOCK_PATH = './pnpm-lock.yaml'
-const MANIFEST_WORKSPACE_PATH = './pnpm-workspace.yaml'
 
 class ReleaseItPnpmPlugin extends Plugin {
   static isEnabled() {
@@ -57,41 +50,6 @@ class ReleaseItPnpmPlugin extends Plugin {
       'ci': options.ci,
       'preRelease': options.preRelease,
     })
-  }
-
-  async init() {
-    const { name, version, private: isPrivate } = readJSON(path.resolve(MANIFEST_PATH))
-    this.setContext({ name, version })
-
-    const updates = [{ entry: MANIFEST_PATH, name, version, isPrivate: !!isPrivate }]
-
-    if (hasAccess(MANIFEST_WORKSPACE_PATH)) {
-      const content = fs.readFileSync(path.resolve(MANIFEST_WORKSPACE_PATH), 'utf8')
-      const workspaceInfo = parse(content)
-
-      const { packages } = workspaceInfo
-      if (!packages || !Array.isArray(packages))
-        throw new Error('Invalid pnpm-workspace.yaml: packages field is missing or not an array')
-
-      const entries = fg.globSync(
-        packages.map(pkg => `${pkg}${MANIFEST_PATH.slice(1)}`),
-        {
-          ignore: ['**/node_modules/**'],
-        },
-      )
-
-      for (const entry of entries) {
-        const { name, version, private: isPrivate } = readJSON(entry)
-        updates.push({ entry, name, version, isPrivate: !!isPrivate })
-      }
-    }
-
-    this.log.info(`Found ${updates.length} packages to update`)
-    this.setContext({ updates })
-  }
-
-  getName() {
-    return this.getContext('name')
   }
 
   getIncrementedVersionCI(options) {
@@ -117,16 +75,6 @@ class ReleaseItPnpmPlugin extends Plugin {
       })
       return semver.valid(result.newVersion)
     }
-
-    const tags = await gitSemverTags()
-    const latestTagVersion = tags[0]
-    const { version } = this.getContext()
-    if (
-      typeof version === 'string'
-      && semver.valid(version)
-      && semver.gt(version, latestTagVersion ?? '0.0.0')
-    )
-      return semver.valid(version)
 
     try {
       const result = await conventionalRecommendedBump({
@@ -166,37 +114,24 @@ class ReleaseItPnpmPlugin extends Plugin {
   }
 
   async bump(newVersion) {
-    const { updates } = this.getContext()
-    if (updates.length === 0)
-      return false
-
-    for (const update of updates) {
-      const { entry, name, version } = update
-
-      if (version === newVersion)
-        continue
-
-      this.log.info(`Package ${name} with version ${version} will be bumped to ${newVersion}`)
-
-      if (!this.options['dry-run']) {
-        const pkg = fs.readFileSync(entry, 'utf8')
-        const updatedPkg = pkg.replace(
-          /"version":\s*".*"/,
-          `"version": "${newVersion}"`,
-        )
-        fs.writeFileSync(entry, updatedPkg)
-      }
-    }
-
-    if (updates.some(update => !update.isPrivate)) {
-      await this.step({
-        task: async () => {
-          await this.exec(`pnpm -r publish --access public --no-git-checks`)
-        },
-        label: 'Publishing packages',
-        prompt: 'publish',
+    if (!this.options['dry-run']) {
+      await versionBump({
+        commit: false,
+        tag: false,
+        push: false,
+        confirm: false,
+        recursive: true,
+        release: newVersion,
       })
     }
+
+    await this.step({
+      task: async () => {
+        await this.exec(`pnpm -r publish --access public --no-git-checks`)
+      },
+      label: 'Publishing packages',
+      prompt: 'publish',
+    })
   }
 
   async release() {
