cc5bf70816044eb95428f6cc4695e6e5a09df641
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-05-06 09:50:37 +0800

    update

--- src/index.js
+++ src/index.js
@@ -232,7 +232,7 @@ class ReleaseItPnpmPlugin extends Plugin {
   }
 
   async release() {
-    if (this.options?.pnpm?.disableRelease || !this.options?.github?.release) 
+    if (this.options?.disableRelease || this.options?.pnpm?.disableRelease || !this.options?.github?.release)
       return
 
     await this.step({
